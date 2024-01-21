8e2bb9c4f20d2d24b4c4e719680ed888a0c212a4
Wanderson Kenedy <wandersonkenedy13@gmail.com>
2024-01-21 12:08:35 -0300

    refactor: add route soma

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/soma", (req, res) => {
+  const soma = 100 + 1;
+
+  res.send({ soma });
 });
 
 app.listen(3000);
